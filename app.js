e37af395c2fec8a833fc492795efb6dc67924300
RathlavathRamesh <rameshrguktbasar@gmailcom>
2023-01-30 10:51:06 +0000

    PlayerDetails

--- app.js
+++ app.js
@@ -1,109 +1,153 @@
 const express = require("express");
-const app = express();
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 
-let db = null;
+const databasePath = path.join(__dirname, "cricketMatchDetails.db");
 
-const dbPath = path.join(__dirname, "cricketMatchDetails.db");
+const app = express();
 
-const initilizeDb = async () => {
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
   try {
-    db = await open({
-      filename: dbPath,
+    database = await open({
+      filename: databasePath,
       driver: sqlite3.Database,
     });
-    app.listen(3000, () => {
-      console.log("Server Is Running at Ramesh http://localhost:3000/");
-    });
-  } catch (er) {
-    console.log(`Db Error Is ${er.message}`);
+
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
     process.exit(1);
   }
 };
 
-initilizeDb();
+initializeDbAndServer();
 
-//GET Player API
-
-const getCamelp = (each) => {
+const convertPlayerDbObjectToResponseObject = (dbObject) => {
   return {
-    playerId: each.player_id,
-    playerName: each.player_name,
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
   };
 };
-app.get(" /players/", async (request, response) => {
-  const getquary = `
-    SELECT 
-    * 
-    FROM 
-    player_details
-    ORDER BY 
-      player_id;
-    `;
-  const result = await db.all(getquary);
-  const final = [];
-  for (let each of result) {
-    let one = getCamelp(each);
-    final.push(one);
-    console.log(one);
-  }
-  response.send(final);
+
+const convertMatchDetailsDbObjectToResponseObject = (dbObject) => {
+  return {
+    matchId: dbObject.match_id,
+    match: dbObject.match,
+    year: dbObject.year,
+  };
+};
+
+app.get("/players/", async (request, response) => {
+  const getPlayerQuery = `
+    SELECT
+      *
+    FROM
+      player_details;`;
+  const playersArray = await database.all(getPlayerQuery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertPlayerDbObjectToResponseObject(eachPlayer)
+    )
+  );
 });
 
-//GET SPECIFIC PLAYER
-app.get(" /players/:playerId/", async (request, response) => {
+app.get("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
-  const getplayer = ` 
+  const getPlayerQuery = `
     SELECT 
-    * 
+      *
     FROM 
-    player_details
-    WHERE player_id=${playerId};
-    `;
-  const result = await db.get(getplayer);
-  const final_result = getCamelp(result[0]);
-  response.send(final_result);
+      player_details 
+    WHERE 
+      player_id = ${playerId};`;
+  const player = await database.get(getPlayerQuery);
+  response.send(convertPlayerDbObjectToResponseObject(player));
 });
-
-//ubdate Player API
 
 app.put("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const { playerName } = request.body;
-  const quary = ` 
-          UPDATE 
-           player_details
-           SET 
-            player_name='${playerName}'
-            WHERE player_id=${playerId};
-           `;
-  await db.run(quary);
+  const updatePlayerQuery = `
+  UPDATE
+    player_details
+  SET
+    player_name ='${playerName}'
+  WHERE
+    player_id = ${playerId};`;
+
+  await database.run(updatePlayerQuery);
   response.send("Player Details Updated");
 });
 
-// GET MATCH DETAILS
-const getMatchCamel = (match) => {
-  return {
-    matchId: match.match_id,
-    match: match.match,
-    year: year,
-  };
-};
 app.get("/matches/:matchId/", async (request, response) => {
   const { matchId } = request.params;
-  const getmatchuary = ` 
-         SELECT 
-         * 
-         FROM 
-         match_details
-         WHERE match_id=${matchId};
-    `;
-  const result = await db.run(getmatchuary);
-  const final = getMatchCamel(result[0]);
-  response.send(final);
+  const matchDetailsQuery = `
+    SELECT
+      *
+    FROM
+      match_details
+    WHERE
+      match_id = ${matchId};`;
+  const matchDetails = await database.get(matchDetailsQuery);
+  response.send(convertMatchDetailsDbObjectToResponseObject(matchDetails));
 });
 
-//
+app.get("/players/:playerId/matches/", async (request, response) => {
+  const { playerId } = request.params;
+  const getPlayerMatchesQuery = `
+    SELECT
+      *
+    FROM player_match_score 
+      NATURAL JOIN match_details
+    WHERE
+      player_id = ${playerId};`;
+  const playerMatches = await database.all(getPlayerMatchesQuery);
+  response.send(
+    playerMatches.map((eachMatch) =>
+      convertMatchDetailsDbObjectToResponseObject(eachMatch)
+    )
+  );
+});
+
+app.get("/matches/:matchId/players", async (request, response) => {
+  const { matchId } = request.params;
+  const getMatchPlayersQuery = `
+    SELECT
+      *
+    FROM player_match_score
+      NATURAL JOIN player_details
+    WHERE
+      match_id = ${matchId};`;
+  const playersArray = await database.all(getMatchPlayersQuery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertPlayerDbObjectToResponseObject(eachPlayer)
+    )
+  );
+});
+
+app.get("/players/:playerId/playerScores/", async (request, response) => {
+  const { playerId } = request.params;
+  const getmatchPlayersQuery = `
+    SELECT
+      player_id AS playerId,
+      player_name AS playerName,
+      SUM(score) AS totalScore,
+      SUM(fours) AS totalFours,
+      SUM(sixes) AS totalSixes
+    FROM player_match_score
+      NATURAL JOIN player_details
+    WHERE
+      player_id = ${playerId};`;
+  const playersMatchDetails = await database.get(getmatchPlayersQuery);
+  response.send(playersMatchDetails);
+});
+
 module.exports = app;
